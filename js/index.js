a723f4f1631bf0d8844c2312df9491ee7ee3c098
Pingorchoo <manuelalonsomartinezvargas@gmail.com>
2023-05-28 09:53:24 -0500

    The functionality of textarea resizing has been removed due to UX considerations.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 import { handleEncryptionButtons } from "./encrypt.js";
-import { reziseTextArea } from "./handleHeightTextArea.js";
 import { handleTheme } from "./handleTheme.js";
 import { handleInputAdvice } from "./inputAdviceHandle.js";
 document.addEventListener("DOMContentLoaded", (e) => {
-    reziseTextArea()
     handleEncryptionButtons()
     handleInputAdvice()
     handleTheme()
